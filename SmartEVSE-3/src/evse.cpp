053ea99e6a776ed0ac2d689cb4bd8c2500230cae
jonesPD <peter_deurenberg@hotmail.com>
2024-10-01 21:36:40 +0200

    More general mode off LED color implementation: - Removes MQTT configurable parameter '/Set/CheapTariffIndicator' - Removes MQTT publication of '/CheapTariffIndicator' - Adds MQTT configurable parameter '/Set/ColorOff' which takes an RGB value 'R:G:B' - Adds MQTT publication of '/ColorOff'

--- SmartEVSE-3/src/evse.cpp
+++ SmartEVSE-3/src/evse.cpp
@@ -255,7 +255,9 @@ int homeBatteryLastUpdate = 0; // Time in milliseconds
 char *downloadUrl = NULL;
 int downloadProgress = 0;
 int downloadSize = 0;
-uint Cheap_tariff_ahead = 0;                                               // set by EXTERNAL logic through MQTT/REST to indicate cheap tariffs ahead until unix time indicated
+// set by EXTERNAL logic through MQTT/REST to indicate cheap tariffs ahead until unix time indicated
+uint8_t ColorOff[3]={0, 0, 0};
+
 //#define FW_UPDATE_DELAY 30        //DINGO TODO                                            // time between detection of new version and actual update in seconds
 #define FW_UPDATE_DELAY 3600                                                    // time between detection of new version and actual update in seconds
 uint16_t firmwareUpdateTimer = 0;                                               // timer for firmware updates in seconds, max 0xffff = approx 18 hours
@@ -456,17 +458,9 @@ void BlinkLed(void * parameter) {
             BluePwm = 0;
 #endif //ENABLE_OCPP
         } else if (Access_bit == 0 || State == STATE_MODEM_DENIED) {
-            if (Cheap_tariff_ahead > time(NULL)) {                  // No access, indicates with BLUE that a cheap tariff is ahead (until current time exceeds set time)
-                RedPwm = 0;
-                GreenPwm = 0;
-                BluePwm = 255;
-                LedPwm = 128;
-            } else {                                                // No Access, LEDs off
-                RedPwm = 0;
-                GreenPwm = 0;
-                BluePwm = 0;
-                LedPwm = 0; 
-            }
+            RedPwm = ColorOff[0];
+            GreenPwm = ColorOff[1];
+            BluePwm = ColorOff[2];
         } else {                                                                // State A, B or C
     
             if (State == STATE_A) {
@@ -2840,8 +2834,16 @@ void mqtt_receive_callback(const String topic, const String payload) {
             preferences.putString("RequiredEVCCID", String(RequiredEVCCID));
             preferences.end();
         }
-    } else if (topic == MQTTprefix + "/Set/CheapTariffIndicator") {
-        Cheap_tariff_ahead = payload.toInt();
+    } else if (topic == MQTTprefix + "/Set/ColorOff") {
+        int32_t R, G, B;
+        int n = sscanf(payload.c_str(), "%d:%d:%d", &R, &G, &B);
+
+        // R,G,B is between 0..255
+        if (n == 3 && (R >= 0 && R < 256) && (G >= 0 && G < 256) && (B >= 0 && B < 256)) {
+            ColorOff[0] = R;
+            ColorOff[1] = G;
+            ColorOff[2] = B;
+        }
     }
 
     // Make sure MQTT updates directly to prevent debounces
@@ -3078,7 +3080,7 @@ void mqttPublishData() {
         MQTTclient.publish(MQTTprefix + "/OCPP", OcppMode ? "Enabled" : "Disabled", true, 0);
         MQTTclient.publish(MQTTprefix + "/OCPPConnection", (OcppWsClient && OcppWsClient->isConnected()) ? "Connected" : "Disconnected", false, 0);
 #endif //ENABLE_OCPP
-        MQTTclient.publish(MQTTprefix + "/CheapTariffIndicator", (Cheap_tariff_ahead>0) ? String(Cheap_tariff_ahead) : "Disabled", true, 0);
+        MQTTclient.publish(MQTTprefix + "/LEDColorOff", String(ColorOff[0])+","+String(ColorOff[1])+","+String(ColorOff[2]), true, 0);
 }
 #endif
 
