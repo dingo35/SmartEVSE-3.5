83259a42eb9d6c0ad70be6a7b642115b84c956cd
dingo35 <spaarder@hotmail.nl>
2024-12-24 13:45:17 +0100

    common.cpp: finish integration of setState

--- SmartEVSE-3/src/common.cpp
+++ SmartEVSE-3/src/common.cpp
@@ -365,7 +365,6 @@ void setState(uint8_t NewState) { //c
             LeaveModemDoneStateTimer = 5;                                       // Disconnect CP for 5 seconds, restart charging cycle but this time without the modem steps.
 #endif
             break;
-#ifdef SMARTEVSE_VERSION //v3 and v4
         case STATE_B:
 #if MODEM
             CP_ON;
@@ -377,6 +376,9 @@ void setState(uint8_t NewState) { //c
             timerAlarmWrite(timerA, PWM_95, false);                             // Enable Timer alarm, set to diode test (95%)
 #endif
             SetCurrent(ChargeCurrent);                                          // Enable PWM
+#ifndef SMARTEVSE_VERSION //CH32
+            TIM1->CH4CVR = PWM_96;                                              // start ADC sampling at 96% (Diode Check)
+#endif
             break;
         case STATE_C:                                                           // State C2
             ActivationMode = 255;                                               // Disable ActivationMode
@@ -397,10 +399,12 @@ void setState(uint8_t NewState) { //c
             LCDTimer = 0;
             break;
         case STATE_C1:
-            SetCPDuty(1024);                                                    // PWM off,  channel 0, duty cycle 100%
 #ifdef SMARTEVSE_VERSION //v3
+            SetCPDuty(1024);                                                    // PWM off,  channel 0, duty cycle 100%
             timerAlarmWrite(timerA, PWM_100, true);                             // Alarm every 1ms, auto reload
-#endif                                                                          // EV should detect and stop charging within 3 seconds
+#else //CH32                                                                          // EV should detect and stop charging within 3 seconds
+            TIM1->CH1CVR = 1000;                                                // Set CP output to +12V
+#endif
             C1Timer = 6;                                                        // Wait maximum 6 seconds, before forcing the contactor off.
             ChargeDelay = 15;
             break;
@@ -417,33 +421,6 @@ void setState(uint8_t NewState) { //c
 #endif
 
     // BacklightTimer = BACKLIGHT;                                                 // Backlight ON
-}
-#else //CH32
-        case STATE_B:
-            CONTACTOR1_OFF;
-            CONTACTOR2_OFF;
-            SetCurrent(ChargeCurrent);                                          // Enable CP PWM
-            TIM1->CH4CVR = PWM_96;                                              // start ADC sampling at 96% (Diode Check)
-            break;
-        case STATE_C:                                                           // State C2
-            ActivationMode = 255;                                               // Disable ActivationMode
-                CONTACTOR1_ON;                                                  // Contactor1 ON
-                CONTACTOR2_ON;                                                  // Contactor2 ON
-            break;
-        case STATE_C1:
-            TIM1->CH1CVR = 1000;                                                // Set CP output to +12V
-            C1Timer = 6;                                                        // Wait maximum 6 seconds, before forcing the contactor off.
-            ChargeDelay = 15;
-            break;
-        default:
-            break;
-    }
-
-//    BalancedState[0] = NewState;
-    State = NewState;
-//    printf("state set to:%s\n", getStateName(State));
 
 #endif //SMARTEVSE_VERSION
 }
-#endif //SMARTEVSE_VERSION
-
