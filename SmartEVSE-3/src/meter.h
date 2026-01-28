1d072cf0a3971f95123b56a60a1bc2e267241e7b
dingo35 <spaarder@hotmail.nl>
2026-01-28 10:55:01 -0500

    meter.h, meter.cpp: measure PeakPower for Capacity rules in Flanders

--- SmartEVSE-3/src/meter.h
+++ SmartEVSE-3/src/meter.h
@@ -97,9 +97,15 @@ class Meter {
     int32_t EnergyMeterStart;                                                   // kWh meter value is stored once EV is connected to EVSE (Wh)
     uint8_t ResetKwh;                                                           // if set, reset kwh meter at state transition B->C
                                                                                 // cleared when charging, reset to 1 when disconnected (state A)
+    // capacity variables
+    int32_t CurrentPeriodStartEnergy;                                           // the value of Import_active_energy at the start of the energy period
+    int32_t Peak_Period_Power;                                                  // the peak of the average power in CapacityPeriodSeconds in the current month
+
     // constructor
     Meter(uint8_t type, uint8_t address, uint8_t timeout);
+
     void UpdateEnergies();
+    void UpdateCapacity();
     void ResponseToMeasurement(struct ModBus MB);
     void CalcImeasured(void);
     void setTimeout(uint8_t Timeout);
