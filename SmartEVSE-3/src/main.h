b35b3fe7948549ef4367abceaad7cf85cf6a6b0d
dingo35 <spaarder@hotmail.nl>
2026-02-07 08:52:01 -0500

    add webpage to insert capacity intervals

--- SmartEVSE-3/src/main.h
+++ SmartEVSE-3/src/main.h
@@ -349,6 +349,11 @@ enum EnableC2_t { NOT_PRESENT, ALWAYS_OFF, SOLAR_OFF, ALWAYS_ON, AUTO };
 extern EnableC2_t EnableC2;
 enum CapacityMode_t { CAP_DISABLED, FIXED, PERIODS, FLANDERS };
 extern CapacityMode_t CapacityMode;
+typedef struct CapacityNode {
+    uint16_t     start_minutes;     // Minutes since midnight (0–1439)
+    int32_t      max_power_watts;   // Allowed power in watts
+    struct CapacityNode* next;
+} CapacityNode;
 
 struct Node_t {
     uint8_t Online;
