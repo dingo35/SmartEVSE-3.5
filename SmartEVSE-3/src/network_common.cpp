2d95cf08bdf3604a4dd243dab414dd996388142a
dingo35 <spaarder@hotmail.nl>
2026-06-30 08:49:30 +0200

    network_common.cpp: get rid of setSleep since it is suspected to influence reconnect behaviour negatively

--- SmartEVSE-3/src/network_common.cpp
+++ SmartEVSE-3/src/network_common.cpp
@@ -2366,7 +2366,6 @@ void WiFiSetup(void) {
     mg_mgr_init(&mgr);
 
     WiFi.setAutoReconnect(true);                                                // Required for Arduino 3
-    WiFi.setSleep(false);                                                       // to prevent wifi disconnects that are not detected by WiFi.status()
     //WiFi.persistent(true);
     WiFi.onEvent(onWifiEvent);
 
